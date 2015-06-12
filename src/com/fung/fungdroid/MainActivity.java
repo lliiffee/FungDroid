80cec6cc8e0832ba2f5afb30b5713455ea3614d2
hongye <185399@qq.com>
2015-06-12 17:23:51 +0800

    20150612

--- src/com/fung/fungdroid/MainActivity.java
+++ src/com/fung/fungdroid/MainActivity.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
